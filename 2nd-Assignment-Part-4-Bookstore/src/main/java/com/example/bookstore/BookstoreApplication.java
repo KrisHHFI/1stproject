0aff7dd9b91a4626d401d924780aeffd3a6066d0
Kristopher Pepper <Bgp534@myy.haaga-helia.fi>
2022-02-23 13:59:02 +0200

    Third commit C3A3

--- 2nd-Assignment-Part-4-Bookstore/src/main/java/com/example/bookstore/BookstoreApplication.java
+++ 2nd-Assignment-Part-4-Bookstore/src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -18,6 +18,7 @@ public class BookstoreApplication {
 	@Bean
 	public CommandLineRunner bookDemo(BookRepository repository) {
 		return (args) -> {
+			// Each time the program is ran, these book objects are created and fetched
 			System.out.println("save a couple of books");
 			repository.save(new Book("Cats vs Dogs II", "John James", 1999, "12345678", 9.99));
 			repository.save(new Book("A man and his penguin", "Alex Agnes", 1998, "22345678", 10.99));
@@ -29,6 +30,4 @@ public class BookstoreApplication {
 
 		};
 	}
-	
 }
-//to view type- http://localhost:8080/ into the browser
--- 2nd-Assignment-Part-4-Bookstore/src/main/java/com/example/bookstore/web/BookstoreController.java
+++ 2nd-Assignment-Part-4-Bookstore/src/main/java/com/example/bookstore/web/BookstoreController.java
@@ -1,35 +1,51 @@
 package com.example.bookstore.web;
 
-//import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-//import com.example.bookstore.model.Book;
+import com.example.bookstore.model.Book;
 import com.example.bookstore.model.BookRepository;
 
 @Controller
-//@ResponseBody
 public class BookstoreController {
 	
 	@Autowired
 	private BookRepository repository; 
 	
+	// Returns all the Books to the HTML page
 	@RequestMapping(value= {"/", "/booklist"})
 	public String bookList(Model model) {
-	
-		//List<Book> books = repository.findById();
 		model.addAttribute("books", repository.findAll());
-		
 		return "booklist";
 	}
+	
+	// Deletes specific books, which are selected from HTML page
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+    	repository.deleteById(bookId);
+        return "redirect:../booklist";
+    }    
+	
+	// Add Book (Part 1)
+	@RequestMapping(value = "/add")
+    public String addBook(Model model){
+    	model.addAttribute("book", new Book());
+        return "addbook";
+    }     
+	
+	// Add Book (Part 2, inputs from addbook.html, then redirected back to booklist.html)
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book){
+        repository.save(book);
+        return "redirect:booklist";
+    }    
 }
-
-
-// Browser url: localhost:8080/h2-console
 //Browser url: localhost:8080/booklist
+
+// For H2 console
+// Url: localhost:8080/h2-console
 // JDBC url field= jdbc:h2:mem:testdb
