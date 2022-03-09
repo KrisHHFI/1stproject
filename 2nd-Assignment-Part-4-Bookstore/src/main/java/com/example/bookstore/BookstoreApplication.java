b0905da0f7ddc1b037aa6cce755a16e0075df773
Kristopher Pepper <Bgp534@myy.haaga-helia.fi>
2022-03-09 15:30:54 +0200

    Chapter 6 Assignment 2

--- 2nd-Assignment-Part-4-Bookstore/src/main/java/com/example/bookstore/BookstoreApplication.java
+++ 2nd-Assignment-Part-4-Bookstore/src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -9,6 +9,12 @@ import com.example.bookstore.model.Book;
 import com.example.bookstore.model.BookRepository;
 import com.example.bookstore.model.Category;
 import com.example.bookstore.model.CategoryRepository;
+import com.example.bookstore.model.User;
+import com.example.bookstore.model.UserRepository;
+
+/**
+ * This class runs the Book Store Application & initialises a couple books and users
+ **/
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -18,7 +24,7 @@ public class BookstoreApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner bookDemo(CategoryRepository categoryRepository, BookRepository bookRepository) {
+	public CommandLineRunner bookDemo(CategoryRepository categoryRepository, BookRepository bookRepository, UserRepository urepository) {
 		return (args) -> {
 			// Category objects created
 			System.out.println("save a couple of categories");
@@ -28,7 +34,12 @@ public class BookstoreApplication {
 			System.out.println("save a couple of books");
 			bookRepository.save(new Book("Cats vs Dogs II", "John James", 1999, "12345678", 9.99, categoryRepository.findByName("Action").get(0)));
 			bookRepository.save(new Book("A man and his penguin", "Alex Agnes", 1998, "22345678", 10.99, categoryRepository.findByName("Thought Provoking").get(0)));
-			
+			// Create 2 users (basic user and admin). Attributes: username (unique), password, role, email
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", "user@evilcorp.com");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN" , "admin@evilcorp.com");
+			urepository.save(user1);
+			urepository.save(user2);
+			// Prints all books for testing purposes
 			System.out.println("fetch all books");
 			for (Book book : bookRepository.findAll()) {
 				System.out.println(book.toString());
