4f73d62d8732faa1de9b2738e4a7b213b0abe93a
Kristopher Pepper <Bgp534@myy.haaga-helia.fi>
2022-03-21 15:31:40 +0200

    Chapter 7 Database

--- 2nd-Assignment-Part-4-Bookstore/src/main/java/com/example/bookstore/BookstoreApplication.java
+++ 2nd-Assignment-Part-4-Bookstore/src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -13,7 +13,7 @@ import com.example.bookstore.model.User;
 import com.example.bookstore.model.UserRepository;
 
 /**
- * This class runs the Book Store Application & initialises a couple books and users
+ * This class runs the Book Store Application & initialises a couple books and users. Needs internet connection for Amazon cloud.
  **/
 
 @SpringBootApplication
@@ -28,17 +28,20 @@ public class BookstoreApplication {
 		return (args) -> {
 			// Category objects created
 			System.out.println("save a couple of categories");
-			categoryRepository.save(new Category("Action"));
-			categoryRepository.save(new Category("Thought Provoking"));
+			//IMPORTANT- Only save new books the first time you run app. Duplicates cause problems
+			//categoryRepository.save(new Category("Action"));
+			//categoryRepository.save(new Category("Thought Provoking"));
 			// Book objects created
+			//IMPORTANT- Only save new books the first time you run app. Duplicates cause problems
 			System.out.println("save a couple of books");
-			bookRepository.save(new Book("Cats vs Dogs II", "John James", 1999, "12345678", 9.99, categoryRepository.findByName("Action").get(0)));
-			bookRepository.save(new Book("A man and his penguin", "Alex Agnes", 1998, "22345678", 10.99, categoryRepository.findByName("Thought Provoking").get(0)));
+			//bookRepository.save(new Book("Cats vs Dogs II", "John James", 1999, "12345678", 9.99, categoryRepository.findByName("Action").get(0)));
+			//bookRepository.save(new Book("A man and his penguin", "Alex Agnes", 1998, "22345678", 10.99, categoryRepository.findByName("Thought Provoking").get(0)));
 			// Create 2 users (basic user and admin). Attributes: username (unique), password, role, email
 			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", "user@evilcorp.com");
 			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN" , "admin@evilcorp.com");
-			urepository.save(user1);
-			urepository.save(user2);
+			//IMPORTANT- Only save new users the first time you run app. Duplicates cause problems
+			//urepository.save(user1);
+			//urepository.save(user2);
 			// Prints all books for testing purposes
 			System.out.println("fetch all books");
 			for (Book book : bookRepository.findAll()) {
